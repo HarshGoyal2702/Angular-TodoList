42231e9e4c5f915a94218f748580bd92f760d9d9
Harsh Goyal <harshgoyal1331@gmail.com>
2024-07-24 16:35:43 +0530

    Angular Todo App with some Animations and Practice

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'; // Removed NgModel import as it's not needed here
+import { Todo } from './todo';
 import { RouterOutlet } from '@angular/router';
-
+import { CommonModule } from '@angular/common'; 
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import {Dialog, DialogModule, DialogRef} from '@angular/cdk/dialog';
+import {
+  CdkDragDrop,
+  CdkDropList,
+  CdkDragPreview,
+  CdkDrag,
+  moveItemInArray,
+} from '@angular/cdk/drag-drop';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule,CommonModule,MatSlideToggleModule,CdkDropList, CdkDrag, CdkDragPreview,DialogModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'TODO-app';
+  todoValue: string;
+  list: Todo[];
+
+  constructor(public dialog: Dialog) {
+    this.list = [];
+    this.todoValue = '';
+  }
+
+  openDialog(): void {
+    this.dialog.open<string>(CdkDialogStylingExampleDialog);
+  }
+  addItem() {
+    if (this.todoValue.trim() !== '') {
+      const newItem: Todo = {
+        id: Date.now(),
+        value: this.todoValue,
+        isDone: false,
+      };
+      this.list.push(newItem);
+    }
+
+    this.todoValue = '';
+  }
+
+  deleteItem(id: number) {
+    this.list = this.list.filter((item) => item.id !== id);
+  }
+  drop(event: CdkDragDrop<{title: string; poster: string}[]>) {
+    moveItemInArray(this.list, event.previousIndex, event.currentIndex);
+  }
+}
+
+  
+
+export class CdkDialogStylingExampleDialog {
+  constructor(public dialogRef: DialogRef) {}
 }
